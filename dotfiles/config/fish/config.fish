e2968275da92fec5c714b8db3ff3e9fa2cd4f398
GrassSand <grasssand@users.noreply.github.com>
2025-12-16 14:51:17 +0800

    chore(fish): update config and PATH env

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -2,7 +2,7 @@
 
 # Format man pages
 set -gx MANROFFOPT "-c"
-set -gx MANPAGER "sh -c 'col -bx | bat -l man -p --theme=\"OneHalfDark\"'"
+set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 if not type -q bat
     set -gx MANPAGER "less -R"
 end
@@ -18,30 +18,60 @@ if status is-interactive
     set -U __done_notification_urgency_level low
     fzf_configure_bindings --variables=
 
+    # Functions
+    # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+    function __history_previous_command
+        switch (commandline -t)
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+        end
+    end
+
+    function __history_previous_command_arguments
+        switch (commandline -t)
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+        end
+    end
+
+    if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+        bind -Minsert ! __history_previous_command
+        bind -Minsert '$' __history_previous_command_arguments
+    else
+        bind ! __history_previous_command
+        bind '$' __history_previous_command_arguments
+    end
+
+    # Fish command history
+    function history
+        builtin history --show-time='%F %T '
+    end
+
+    # Backup: 备份文件到 filename.bak
     function backup --argument filename
         cp $filename $filename.bak
     end
 
     # Copy: 安全递归、保留属性、智能处理目录末尾 /
     function copy
-        # 参数不足直接提示
         if test (count $argv) -lt 2
             echo "Usage: copy <source...> <dest>"
             return 1
         end
 
-        # 取最后一个参数作为目标，其余为源
         set -l dest $argv[-1]
         set -l sources $argv[1..-2]
-
         set -l opts "-a"  # 保留权限/时间戳/符号链接
 
-        # 检查是否为目录到目录的单源模式
         if test (count $sources) = 1; and test -d "$sources[1]"; and test -d "$dest"
             set sources (string trim -r -c '/' -- $sources[1])
         end
 
-        # 防御：文件名可能以 - 开头 → 加 --
         command cp $opts -- $sources $dest
     end
 
@@ -58,25 +88,26 @@ end
 
 ## Useful aliases
 # Replace ls with eza
-alias l="eza -al --color=always --group-directories-first --icons"  # preferred listing
-alias la="eza -al --color=always --group-directories-first --icons" # preferred listing
-alias ls="eza -a --color=always --group-directories-first --icons"  # all files and dirs
-alias ll="eza -l --color=always --group-directories-first --icons"  # long format
-alias lt="eza -aT --color=always --group-directories-first --icons" # tree listing
-alias l.="eza -a | grep -e '^\.'"                                   # show only dotfiles
+alias l="eza -al --color=always --group-directories-first --icons=always"   # preferred listing
+alias la="eza -al --color=always --group-directories-first --icons=always"  # preferred listing
+alias ls="eza -a --color=always --group-directories-first --icons=always"   # all files and dirs
+alias ll="eza -l --color=always --group-directories-first --icons=always"   # long format
+alias lt="eza -aT --color=always --group-directories-first --icons=always"  # tree listing
+alias l.="eza -a | grep -e '^\.'"                                           # show only dotfiles
 
+# Common use
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 
-# Common use
 alias fixpacman="sudo rm /var/lib/pacman/db.lck"
 alias psmem="ps auxf | sort -nr -k 4"
 alias psmem10="ps auxf | sort -nr -k 4 | head -10"
 alias cleanup="sudo pacman -Rns (pacman -Qtdq)"             # Cleanup orphaned packages
 alias jctl="journalctl -p 3 -xb"                            # Get the error messages from journalctl
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Show biggest installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -40 | nl"    # Recent installed packages
 alias cat="bat --paging=never"
-alias cc="cliphist wipe"
-alias x="extract"
+alias cpc="clipse -clear-all"
