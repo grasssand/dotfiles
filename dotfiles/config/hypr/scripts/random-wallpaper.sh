49e7effa42dbf85d991b32fa958aa1035f570435
GrassSand <grasssand@users.noreply.github.com>
2026-01-04 14:58:12 +0800

    chore(hyprland): update configuration

--- dotfiles/config/hypr/scripts/random-wallpaper.sh
+++ dotfiles/config/hypr/scripts/random-wallpaper.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+# CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper wallpaper ,"$WALLPAPER"
